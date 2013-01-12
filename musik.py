805b9de74d1f0b46002d1e308188ae231afdd24a
MusikPolice <jonfritz@gmail.com>
2013-01-12 18:06:43 -0500

    PEP8 compliance corrections

--- musik.py
+++ musik.py
@@ -2,7 +2,6 @@
 
 import signal
 import sys
-import os
 
 from musik import config
 from musik import log
@@ -12,49 +11,49 @@ import musik.web
 
 # cleans up and safely stops the application
 def cleanup(signum=None, frame=None):
-	global log, importThread, app
+    global log, importThread, app
 
-	if type(signum) == type(None):
-		pass
-	else:
-		log.info(u'Signal %i caught, saving and exiting...' % int(signum))
+    if type(signum) == type(None):
+        pass
+    else:
+        log.info(u'Signal %i caught, saving and exiting...' % int(signum))
 
-	log.info(u'Stopping worker threads')
-	if importThread != None:
-		importThread.stop()
-		importThread.join(5)
-		if importThread.isAlive():
-			log.error(u'Failed to clean up importThread')
+    log.info(u'Stopping worker threads')
+    if importThread != None:
+        importThread.stop()
+        importThread.join(5)
+        if importThread.isAlive():
+            log.error(u'Failed to clean up importThread')
 
-	log.info(u'Stopping CherryPy Engine')
-	app.stop()
+    log.info(u'Stopping CherryPy Engine')
+    app.stop()
 
-	log.info(u'Clean up complete')
-	sys.exit(0)
+    log.info(u'Clean up complete')
+    sys.exit(0)
 
 
 # application entry - starts the database connection and dev server
 if __name__ == '__main__':
-	global log, importThread, app
+    global log, importThread, app
 
-	threads = []
+    threads = []
 
-	log = log.Log(__name__)
+    log = log.Log(__name__)
 
-	# TODO: also register for CherryPy shutdown messages
-	log.info(u'Registering for shutdown signals')
-	for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
-		signal.signal(sig, cleanup)
+    # TODO: also register for CherryPy shutdown messages
+    log.info(u'Registering for shutdown signals')
+    for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+        signal.signal(sig, cleanup)
 
-	log.info(u'Starting worker threads')
-	importThread = musik.importer.ImportThread()
-	importThread.start()
-	threads.append(importThread)
+    log.info(u'Starting worker threads')
+    importThread = musik.importer.ImportThread()
+    importThread.start()
+    threads.append(importThread)
 
-	# this is a blocking call
-	log.info(u'Starting Web App')
-	app = musik.web.WebService(threads=threads)
+    # this is a blocking call
+    log.info(u'Starting Web App')
+    app = musik.web.WebService(threads=threads)
 
-	# start the web app
-	port = config.get_server_port()
-	app.start(port=port)
+    # start the web app
+    port = config.get_server_port()
+    app.start(port=port)
