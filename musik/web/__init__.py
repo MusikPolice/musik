15f734b2a211fd21181ebc350156b09949ddca0d
MusikPolice <jonfritz@gmail.com>
2013-01-14 07:52:28 -0500

    PEP8 compliance and some more work on the authentication layer

--- musik/web/__init__.py
+++ musik/web/__init__.py
@@ -1,156 +1,162 @@
+import datetime
+
+import musik
 import musik.config
-import musik.web.api
-import musik.web.application
 from musik import log
 from musik.db import DatabaseWrapper, User
 
 import cherrypy
-from cherrypy.process import wspbus, plugins
+from cherrypy.process import plugins
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+
 class SAEnginePlugin(plugins.SimplePlugin):
-	"""A plugin to help SQLAlchemy bind correctly to CherryPy threads.
-	See http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
-	"""
-	def __init__(self, bus):
-		plugins.SimplePlugin.__init__(self, bus)
-		self.sa_engine = None
-		self.bus.subscribe(u'bind', self.bind)
+    """A plugin to help SQLAlchemy bind correctly to CherryPy threads.
+    See http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
+    """
+    def __init__(self, bus):
+        plugins.SimplePlugin.__init__(self, bus)
+        self.sa_engine = None
+        self.bus.subscribe(u'bind', self.bind)
 
-	def start(self):
-		self.db = DatabaseWrapper()
-		self.sa_engine = self.db.get_engine()
+    def start(self):
+        self.db = DatabaseWrapper()
+        self.sa_engine = self.db.get_engine()
 
-	def stop(self):
-		if self.sa_engine:
-			self.sa_engine.dispose()
-			self.sa_engine = None
+    def stop(self):
+        if self.sa_engine:
+            self.sa_engine.dispose()
+            self.sa_engine = None
 
-	def bind(self, session):
-		session.configure(bind=self.sa_engine)
+    def bind(self, session):
+        session.configure(bind=self.sa_engine)
 
 
 class SATool(cherrypy.Tool):
-	"""A tool to help SQLAlchemy correctly dole out and clean up db connections
-	See http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
-	"""
-	def __init__(self):
-		cherrypy.Tool.__init__(self, 'on_start_resource', self.bind_session, priority=20)
-		self.session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
+    """A tool to help SQLAlchemy correctly dole out and clean up db connections
+    See http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
+    """
+    def __init__(self):
+        cherrypy.Tool.__init__(self, 'on_start_resource', self.bind_session, priority=20)
+        self.session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
 
-	def _setup(self):
-		cherrypy.Tool._setup(self)
-		cherrypy.request.hooks.attach('on_end_resource', self.commit_transaction, priority=80)
+    def _setup(self):
+        cherrypy.Tool._setup(self)
+        cherrypy.request.hooks.attach('on_end_resource', self.commit_transaction, priority=80)
 
-	def bind_session(self):
-		cherrypy.engine.publish('bind', self.session)
-		cherrypy.request.db = self.session
+    def bind_session(self):
+        cherrypy.engine.publish('bind', self.session)
+        cherrypy.request.db = self.session
 
-	def commit_transaction(self):
-		cherrypy.request.db = None
-		try:
-			self.session.commit()
-		except:
-			self.session.rollback()
-			raise
-		finally:
-			self.session.remove()
+    def commit_transaction(self):
+        cherrypy.request.db = None
+        try:
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
+        finally:
+            self.session.remove()
 
 
 class AuthTool(cherrypy.Tool):
-	"""This tool intercepts requests and checks to see if the user is logged in. The cherrypy.request.authorized flag
-	will be set with the results of the test."""
-	log = None
+    """This tool intercepts application requests and checks to see if the user is logged in.
+    The cherrypy.request.authorized flag will be set with the results of the test."""
+    log = None
 
-	def __init__(self):
-		self.log = log.Log(__name__)
-		cherrypy.Tool.__init__(self, 'before_handler', self.auth_check, priority=30)
+    def __init__(self):
+        self.log = log.Log(__name__)
+        cherrypy.Tool.__init__(self, 'before_handler', self.auth_check, priority=30)
 
-	def auth_check(self):
-		"""Checks to see if a username and password combination is set in the session variables. If so, the API is
-		called to ensure that the credentials are still valid."""
-		self.log.info('AuthTool username=%s, password=%s' % (cherrypy.session.get('username'), cherrypy.session.get('password')))
-		if cherrypy.session.get('username') is None or cherrypy.session.get('password') is None:
-			cherrypy.request.authorized = False
-		else:
-			# TODO: call api to validate credentials?
-			cherrypy.request.authorized = True
+    def auth_check(self):
+        """Returns true if the user's credentials have been stored in a session and they can
+        be authenticated"""
+        token = cherrypy.session.get('token')
+
+        if token is not None:
+            musik.web.api.users.check_password(token, None)
+
+        if cherrypy.request.authorized:
+            cherrypy.session['token'] = cherrypy.request.user.token
+            return True
+        else:
+            return False
 
 
 class WebService(object):
-	"""Application entry point for the web interface and restful API
-	"""
-	log = None
-	threads = None
+    """Application entry point for the web interface and restful API
+    """
+    log = None
+    threads = None
 
-	def __init__(self, threads):
-		self.log = log.Log(__name__)
-		self.threads = threads
+    def __init__(self, threads):
+        self.log = log.Log(__name__)
+        self.threads = threads
 
-		# Subscribe specifically to the 'stop' method passed by cherrypy.
-		# This lets us cleanly stop all threads executed by the application.
-		SAEnginePlugin(cherrypy.engine).subscribe()
-		cherrypy.engine.subscribe("stop", self.stop_threads)
+        # Subscribe specifically to the 'stop' method passed by cherrypy.
+        # This lets us cleanly stop all threads executed by the application.
+        SAEnginePlugin(cherrypy.engine).subscribe()
+        cherrypy.engine.subscribe("stop", self.stop_threads)
 
-		# make a database transaction available to every request
-		cherrypy.tools.db = SATool()
+        # make a database transaction available to every request
+        cherrypy.tools.db = SATool()
 
-		# authenticate the user before every request
-		cherrypy.tools.auth = AuthTool()
+        # authenticate the user before every request
+        cherrypy.tools.auth = AuthTool()
 
-		app_config = {'/':
-			{
-				'tools.db.on': True,
-				'tools.auth.on': True,
-				'tools.sessions.on': True,
-				'tools.staticdir.root': musik.config.get_root_directory(),
-			},
-			'/static':
-			{
-				# don't do authentication for static files
-				'tools.auth.on': False,
-				'tools.staticdir.on': True,
-				'tools.staticdir.dir': "static",
-			},
-		}
+        app_config = {'/':
+            {
+                'tools.db.on': True,
+                'tools.auth.on': True,
+                'tools.sessions.on': True,
+                'tools.staticdir.root': musik.config.get_root_directory(),
+            },
+            '/static':
+            {
+                # don't do authentication for static files
+                'tools.auth.on': False,
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': "static",
+            },
+        }
 
-		api_config = {
-			'/':
-			{
-				'tools.db.on': True,
-				'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+        api_config = {
+            '/':
+            {
+                'tools.db.on': True,
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
 
-				# all api calls require that the client passes HTTP basic authentication
-				'tools.auth_basic.on': True,
-				'tools.auth_basic.realm': 'api',
-				'tools.auth_basic.checkpassword': api.users.check_password,
-			},
+                # all api calls require that the client passes HTTP basic authentication
+                'tools.auth_basic.on': True,
+                'tools.auth_basic.realm': 'api',
+                'tools.auth_basic.checkpassword': musik.web.api.users.check_password,
+            },
 
-			'/users':
-			{
-				# anybody can list existing user accounts or create a new one
-				'tools.auth_basic.on': False,
-			}
-		}
+            '/users':
+            {
+                # anybody can list existing user accounts or create a new one
+                'tools.auth_basic.on': False,
+            }
+        }
 
-		cherrypy.tree.mount(application.Musik(), '/', config=app_config)
-		cherrypy.tree.mount(api.API(), '/api', config=api_config)
+        cherrypy.tree.mount(musik.web.application.Musik(), '/', config=app_config)
+        cherrypy.tree.mount(musik.web.api.API(), '/api', config=api_config)
 
-	# a blocking call that starts the web application listening for requests
-	def start(self, port=8080):
-		cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-		cherrypy.config.update({'server.socket_port': port,})
-		cherrypy.engine.start()
-		cherrypy.engine.block()
+    # a blocking call that starts the web application listening for requests
+    def start(self, port=8080):
+        cherrypy.config.update({'server.socket_host': '0.0.0.0', })
+        cherrypy.config.update({'server.socket_port': port, })
+        cherrypy.engine.start()
+        cherrypy.engine.block()
 
-	# stops the web application
-	def stop(self):
-		self.log.info(u"Trying to stop main web application")
-		cherrypy.engine.stop()
+    # stops the web application
+    def stop(self):
+        self.log.info(u"Trying to stop main web application")
+        cherrypy.engine.stop()
 
-	# stop all threads before closing out this thread
-	def stop_threads(self):
-		self.log.info(u"Trying to stop all threads")
-		for thread in self.threads:
-			if thread.is_alive():
-				thread.stop()
+    # stop all threads before closing out this thread
+    def stop_threads(self):
+        self.log.info(u"Trying to stop all threads")
+        for thread in self.threads:
+            if thread.is_alive():
+                thread.stop()
--- musik/web/api/users.py
+++ musik/web/api/users.py
@@ -1,4 +1,5 @@
 import cherrypy
+import datetime
 import json
 
 from musik import log
@@ -7,80 +8,94 @@ from musik.util import DateTimeEncoder
 
 
 def check_password(realm, username, password):
-	"""Verifies that the supplied username and password are valid.
-	If so, a username header is added to the request object"""
-	db = DatabaseWrapper()
-	session = db.get_session()
+    """Returns true if the supplied username and password are valid.
+    The username is first tested as a session token. On failure, it is tested as a username.
+    If either test passes, the cherrypy.request.authorized member is set to True and the
+    cherrypy.request.user member is set to the authenticated user."""
+    db = DatabaseWrapper()
+    session = db.get_session()
+    logg = log.Log(__name__)
 
-	user1 = session.query(User).filter(User.name == username).first()
-	if user1 is None:
-		# bad username
-		return False
+    if username is not None:
+        logg.info('AuthTool token=%s' % username)
+        user = session.query(User).filter(User.token == username and User.token_expires > datetime.datetime.utcnow()).first()
+        if user is not None:
+            user.update_token_expiry()
+            session.commit()
+            cherrypy.request.user = user
 
-	user2 = User(username, password)
-	if user1.passhash == user2.passhash:
-		# valid user
-		cherrypy.request.headers['username'] = username
-		return True
-	else:
-		# bad password
-		return False
+    if cherrypy.request.user is None and username is not None and password is not None:
+        logg.info('AuthTool username=%s, password=%s' % (username, password))
+        user = session.query(User).filter(User.name == username).first()
+        if user is not None and user.passhash == user.password_hash(username, password):
+            cherrypy.request.user = user
 
-
-class UserAccounts():
-	log = None
-	exposed = True
-
-	def __init__(self):
-		self.log = log.Log(__name__)
-
-
-	def POST(self):
-		"""Creates a new user account with the specified user name and password"""
-		cherrypy.response.headers['Content-Type'] = 'application/json'
-
-		# ensure that a valid username and password were specified
-		request = json.loads(cherrypy.request.body.read())
-		self.log.info(request)
-		username = request['username']
-		password = request['password']
-
-		if username is None or username == '':
-			raise cherrypy.HTTPError(400, "Unspecified username")
-
-		if password is None or password == '':
-			raise cherrypy.HTTPError(400, "Unspecified password")
-
-		# make sure that username doesn't already exist
-		if cherrypy.request.db.query(User).filter(User.name == username).first() is not None:
-			raise cherrypy.HTTPError(400, "Username already exists")
-
-		# create the user
-		user = User(username, password)
-		cherrypy.request.db.add(user)
-		cherrypy.request.db.commit()
-
-		# this is an http 200 ok with no data
-		return json.dumps(None)
-
-	def GET(self):
-		"""Returns a list of registered usernames"""
-		cherrypy.response.headers['Content-Type'] = 'application/json'
-
-		usernames = [u.name for u in cherrypy.request.db.query(User).all()]
-		return json.dumps(usernames)
+    if cherrypy.request.user is not None:
+        cherrypy.request.authorized = True
+        return True
+    else:
+        cherrypy.request.authorized = False
+        return False
 
 
 class CurrentUser():
-	log = None
-	exposed = True
+    exposed = True
 
-	def __init__(self):
-		self.log = log.Log(__name__)
+    def GET(self):
+        """Gets information about the user that is currently logged in.
+        Note that the password hash is not returned."""
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        return json.dumps(cherrypy.request.user.as_dict(), cls=DateTimeEncoder)
 
-	def GET(self):
-		"""Gets information about the currently logged in user"""
-		cherrypy.response.headers['Content-Type'] = 'application/json'
+    def PUT(self):
+        """Generates a new session token for the user that was authenticated in
+        the pre-request hook. Returns account information. Note that the password
+        hash is not returned."""
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        cherrypy.request.user.generate_token()
+        cherrypy.request.db.commit()
+        return json.dumps(cherrypy.request.user.as_dict(), cls=DateTimeEncoder)
 
-		user = cherrypy.request.db.query(User).filter(User.name == cherrypy.request.headers['username']).first()
-		return json.dumps(user.as_dict(), cls=DateTimeEncoder)
+
+class UserAccounts():
+    """The methods in this class are NOT protected by HTTP authentication"""
+    log = None
+    exposed = True
+
+    def __init__(self):
+        self.log = log.Log(__name__)
+
+    def POST(self):
+        """Creates a new user account with the specified user name and password"""
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+
+        # ensure that a valid username and password were specified
+        request = json.loads(cherrypy.request.body.read())
+        username = request['username']
+        password = request['password']
+
+        if username is None or username == '':
+            raise cherrypy.HTTPError(400, "Unspecified username")
+
+        if password is None or password == '':
+            raise cherrypy.HTTPError(400, "Unspecified password")
+
+        # make sure that username doesn't already exist
+        if cherrypy.request.db.query(User).filter(User.name == username).first() is not None:
+            raise cherrypy.HTTPError(400, "Username already exists")
+
+        # create the user
+        user = User(username, password)
+        cherrypy.request.db.add(user)
+        cherrypy.request.db.commit()
+
+        # return the user to the calling function.
+        # Note that the password hash is not returned
+        return json.dumps(user.as_dict, cls=DateTimeEncoder)
+
+    def GET(self):
+        """Returns a list of registered usernames"""
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+
+        usernames = [u.name for u in cherrypy.request.db.query(User).all()]
+        return json.dumps(usernames)
