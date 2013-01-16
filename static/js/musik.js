3b17a72621ef0ef93b22af4c000f85ee52a600c3
MusikPolice <jonfritz@gmail.com>
2013-01-16 08:52:02 -0500

    Added login form to page on first load, hooked submit button on login form up to the current user's login function. Next steps are to make that function call the API and throw a login event on success.

--- static/js/musik.js
+++ static/js/musik.js
@@ -1,17 +1,46 @@
+var musik;
+
 $(function() {
     //logical model of a user
-    var User = Backbone.Model.extend({});
+    var User = Backbone.Model.extend({
+        login: function(username, password) {
+            alert('Login Called with username ' + username + ' password ' + password);
+            return true;
+        }
+    });
 
     //visual representation of a user
     var CurrentUserView = Backbone.View.extend({
         render: function() {
-            this.el = ich.user(this.model.toJSON());
+            this.el = ich.currentUser(this.model.toJSON());
             return this;
         }
-    })
+    });
+
+    //visual representation of the login form
+    var LoginView = Backbone.View.extend({
+        el: $('.content'),
+
+        events: {
+            'click button': 'balls'
+        },
+
+        render: function() {
+            this.el = ich.login();
+            return this;
+        },
+
+        balls: function() {
+            currentUser.login($('input#username').val(), $('input#password').val());
+            return false;
+        }
+    });
 
     //drop the user into the DOM
     var currentUser = new User({username: "hello world"});
-    var view = new CurrentUserView({model: currentUser});
-    $('.content').append(view.render().el);
+    $('header').append((new CurrentUserView({model: currentUser})).render().el);
+    $('.content').html((new LoginView()).render().el);
+
+    //make important objects visible to the debug console
+    musik = {'currentUser': currentUser}
 });
