e32b5a0a5f39e419393710a9e9f2bb3826883a1a
MusikPolice <jonfritz@gmail.com>
2013-04-02 07:59:34 -0400

    Modified python routing so that all urls that don't start with /api are redirected to index page. This allows ember to handle url management on the client side

--- musik/web/application.py
+++ musik/web/application.py
@@ -8,8 +8,9 @@ templates = TemplateLookup(
 
 
 class Musik:
-    """An ember.js front-end implementation for the Musik API"""
+    """An ember.js front-end implementation for the Musik API.
+    In practice, *params is ignored as ember.js handles all url redirection"""
     @cherrypy.expose
-    def index(self):
+    def default(self, *params):
         """Renders the index.html template."""
         return templates.get_template('musik.html').render()
